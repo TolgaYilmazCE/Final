27ee3901099baab5fb4af2609d1cdf438f714e52
Toniii10 <1501024003@stu.iku.edu.tr>
2021-06-05 11:43:22 +0300

    7th commit for Save student Controller and correction for view

--- Basic-app/app/Http/Controllers/StudentController.php
+++ Basic-app/app/Http/Controllers/StudentController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    function Save()
+    {
+        return view('studentsave');
+    }
+
+    function StudentInfo(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'surname' => 'required',
+            'departmant' => 'required'
+        ]);
+
+        $Student = new Student();
+        $Student->name = $request->name;
+        $Student->surname = $request->surname;
+        $Student->departmant = $request->departmant;
+        $save = $Student->save();
+
+        if ($save) {
+            return back()->with('success', 'Student saved successfully');
+        } else {
+            return back()->with('fail', 'Something wrong, try again.');
+        }
+    }
 }
