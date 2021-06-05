e11f663e7286abe5abc09cfeab2f065847e19486
Toniii10 <1501024003@stu.iku.edu.tr>
2021-06-05 11:10:55 +0300

    6th commit for Save student view

--- Basic-app/resources/views/student.blade.php
+++ Basic-app/resources/views/student.blade.php
@@ -4,11 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+    <title>SaveStudent</title>
+    <!-- Bootstrap from CDN -->
+    <link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.cs
+s">
+    <script
+src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js">
+</script>
 </head>
 <body>
+    <div class="container">
+        <div class="row" style="margin-top: 45px;">
+            <div class="col-md-4 offset-md-4">
+                <h4>Save</h4>
+                <hr>
+                <form action="" method="post">
+                    <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" name="name"
+placeholder="Enter name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Surname</label>
+                    <input type="text" class="form-control" name="surname"
+placeholder="Enter surname">
+                </div>
+                <div class="form-group">
+                    <label for="departmant">departmant</label>
+                     <input type="departmant" class="form-control"
+name="departmant" placeholder="Enter departmant">
+                </div>
+                <button type="submit" class="form-control">save </button>
+                <br>
 
-    <h1>Tolga Yılmaz-1501024003-Final-App</h1>
-
-</body>
+             </form>
+         </div>
+        </div>
+      </div>
+    </body>
 </html>
